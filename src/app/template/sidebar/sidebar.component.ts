e636038d06b40f25b2fde78bfd8d948240ae2771
rodolfoHOk <rodolfo.okino@gmail.com>
2021-02-26 18:03:40 -0300

    criado o template

---
+++ src/app/template/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as jQuery from 'jquery'; // contornando erro do import
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    (function($) {
+      "use strict";
+  
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this.getAttribute("href") === path) { // contornar erro ts2339
+                  $(this).addClass("active");
+              }
+          });
+  
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
+
+}
